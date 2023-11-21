fb8e0513c2c0889c098e9fd4f36569f791039481
joelnyongesa <joelnyongesa148@gmail.com>
2023-11-22 00:18:09 +0300

    feat: components including home, navbar and welcome

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar"
+import Welcome from "./Welcome"
+function App() {
+  return (
+    <div className="font-primary bg-primary text-secondary">
+      <Navbar />
+      <Welcome />
+    </div>
+  )
+}
+
+export default App
