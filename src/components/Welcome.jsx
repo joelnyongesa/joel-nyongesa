cf65708d1f55d109cc1216f50fdea5d09fa6a99c
joelnyongesa <joelnyongesa148@gmail.com>
2023-11-22 00:44:39 +0300

    Updated files for deployment

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,7 @@
 function Welcome() {
   return (
     <div className="flex flex-col p-5">
-        <h1 className="text-4xl py-5">Hello, I'm Joel Nyongesa.</h1>
+        <h1 className="text-4xl py-5">Hello, I am  Joel Nyongesa.</h1>
         <h2 className="text-3xl py-5">A Full Stack Developer</h2>
         <p>I bring ideas to life by developing full stack web applications that solve organizational and real world problems.</p>
 
